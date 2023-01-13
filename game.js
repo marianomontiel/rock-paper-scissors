d41dbbe23c7e2ecf232d116babd6fa408bad3e81
marianomontiel <67610069+marianomontiel@users.noreply.github.com>
2023-01-13 15:36:38 -0300

    Add main functions

---
+++ game.js
@@ -0,0 +1,66 @@
+// run a function to start game
+// randomly choose who starts
+// The player chooses what to play in the row. (computer chooses randonmly )
+// After row repeat 3 times
+// The player with more wins within 3 rows wins
+
+var computerChoice;
+var humanChoice;
+var humanScore;
+var computerScore;
+
+
+function getComputerChoice() {
+  computerChoice = Math.floor(Math.random() * 3) + 1;
+  if (computerChoice === 1) {
+    computerChoice = "Rock";
+  } else if (computerChoice === 2) {
+    computerChoice = "Paper"
+  } else { computerChoice = "Scissors" }
+  console.log(computerChoice);
+}
+
+function getHumanChoice() {
+  humanChoice = prompt();
+  if (humanChoice == "Rock") {
+    humanChoice = 1;
+  } else if (humanChoice == "Paper") {
+    humanChoice = 2
+  } else { humanChoice = 3 }
+  console.log(humanChoice);
+}
+
+function getPlayerFirstTurn() {
+  let turn = Math.floor(Math.random() * 2) + 1;
+  if (turn === 1) {
+    getHumanChoice();
+    getComputerChoice();
+  } else {
+    getComputerChoice();
+    getHumanChoice();
+  }
+}
+
+/* Three possible scenarios:
+1) tie; 2) rock crushes scissors; 3) paper covers rock; 4) scissors cut paper */
+function whoWins () {
+    if (humanChoice == computerChoice) {
+      row ()
+    } 
+    else if (humanChoice == 3 && computerChoice == 2 ||        computerChoice == 3 && humanChoice == 2) {
+      if (humanChoice > computerChoice) {
+        humanScore = +1;} else {computerScore = +1;}
+      } 
+    else if (humanChoice == 1 && computerChoice == 3 ||        computerChoice == 3 && humanChoice == 1) {
+      if (humanChoice < computerChoice) {
+        humanScore = +1;} else {computerScore = +1;}
+      }
+    else if (humanChoice == 1 && computerChoice == 2 ||        computerChoice == 2 && humanChoice == 1) {
+      if (humanChoice > computerChoice) {
+        humanScore = +1;} else {computerScore = +1;}
+      }
+}
+
+getPlayerFirstTurn();
+whoWins();
+alert ("Human = "+ humanScore + "Computer = " + computerScore + ".");
